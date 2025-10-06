8cc1f7aa64e5b27c2289ef19313c1819bd048729
HailPapaiWILSON <projetocolab53@gmail.com>
2025-10-06 16:10:12 -0300

    Implementa lógica principal CLI para adição de livros

---
+++ src/main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.sql.SQLException;
+
+public class Main
+{
+    public static void Prompt_Add()
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Nome do Livro: ");
+        String title = scan.nextLine();
+        System.out.println("Autor do Livro: ");
+        String author = scan.nextLine();
+        System.out.println("url do livro: ");
+        String url = scan.nextLine();
+
+        try
+        {
+            database.Add_Book(title, author, url);
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Erro ao adicionar o livro: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            database.Create_Tables();
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Erro ao adicionar o livro: " + e.getMessage());
+        }
+
+        Scanner scan = new Scanner(System.in);
+
+        while(true)
+        {
+            System.out.println("----BEM VINDO A ALEXANDRIA----");
+            System.out.println("1. 🌟 Adicionar um livro");
+            System.out.println("2. 🚪 Sair");
+            
+            int choice = scan.nextInt();
+            scan.nextLine();
+
+            if(choice == 1)
+            {
+                Prompt_Add();
+            }
+            else if(choice == 2)
+            {
+                System.out.println("Saindo...");
+                break;
+            }
+        }
+    }
+}
